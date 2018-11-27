5c1a3b89dc8907aea48f91efe53d4e07b587ffb2
Cristhian Antonio Alcalá <31580380+CristhianAntonioA@users.noreply.github.com>
2018-11-26 21:21:05 -0500

    --

--- src/java/encriptar.java
+++ src/java/encriptar.java
@@ -7,6 +7,7 @@
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  *
@@ -34,49 +35,66 @@ public class encriptar implements Serializable {
 //
 //        }
 //    }
-    public static boolean isNumeric(String cadena) {
-
-        boolean resultado;
-
-        try {
-            Integer.parseInt(cadena);
-            resultado = true;
-        } catch (NumberFormatException excepcion) {
-            resultado = false;
-        }
-
-        return resultado;
-    }
-
+//    public static boolean isNumeric(String cadena) {
+//
+//        boolean resultado;
+//
+//        try {
+//            Integer.parseInt(cadena);
+//            resultado = true;
+//        } catch (NumberFormatException excepcion) {
+//            resultado = false;
+//        }
+//
+//        return resultado;
+//    }
     public static void main(String[] args) {
-
-        String cadena = "a";
+        String cadena = "HOLA ";
         int numero;
-
-        if (isNumeric(cadena) == true) {
-            numero = Integer.parseInt(cadena);
-            int cantidad = cadena.length();
-            System.out.println("Contraseña " + cadena);
-            System.out.println("length: " + cantidad);
-            String Resultado = "";
-            for (int i = 0; i < cantidad; i++) {
-                System.out.println("extrae :" + cadena.charAt(i));
-                int suma = Integer.parseInt(String.valueOf(cadena.charAt(i)));
-                suma = suma * 3;
-                Resultado = Resultado + " " + String.valueOf(suma);
-                System.out.println(suma);
-                System.out.println("Numero: " + numero);
-            }
-            System.out.println("resultado final:" + Resultado);
-        } else {
-             int cantidad = cadena.length();
-                 char characterValue = 'a';
-            
-            
-            System.out.println("es texto: " + cadena);
-            char characterValue = cadena.charAt(numero);
-            int asciiValue = (int) characterValue;
-            System.out.println(asciiValue);
+        String resultado = "";
+        int nameLenght = cadena.length(); // contar caracteres
+        for (int i = 0; i < nameLenght; i++) {   // while counting characters if less than the length add one        
+            char character = cadena.charAt(i); // start on the first character
+            int ascii = (int) character; //convert the first character
+//            System.out.println(character + " = " + ascii); // print the character and it's value in ascii
+            resultado = resultado + " " + ascii;
         }
+        System.out.println(resultado);
+        resultado = resultado.substring(1);// borrar el primer espacio
+        resultado = resultado.replace(" ", ",");// remplazo los espacions con ','
+        String[] numerocadena = resultado.split(",");
+        System.out.println(Arrays.toString(numerocadena));
+        for (int i = 0; i < numerocadena.length; i++) {
+            numero = Integer.valueOf(numerocadena[i]);
+            if (numero % 2 == 0) {
+                System.out.println(" es divisible" + numero);
+                numero = numero * 16;
+            }
+            System.out.println(numero); // desmenuzar array
+        }
+        
+        
+        
+
+//        if (isNumeric(cadena) == true) {
+//            numero = Integer.parseInt(cadena);
+//            int cantidad = cadena.length();
+//            System.out.println("Contraseña " + cadena);
+//            System.out.println("length: " + cantidad);
+//            String Resultado = "";
+//            for (int i = 0; i < cantidad; i++) {
+//                System.out.println("extrae :" + cadena.charAt(i));
+//                int suma = Integer.parseInt(String.valueOf(cadena.charAt(i)));
+//                suma = suma * 3;
+//                Resultado = Resultado + " " + String.valueOf(suma);
+//                System.out.println(suma);
+//                System.out.println("Numero: " + numero);
+//            }
+//            System.out.println("resultado final:" + Resultado);
+//        } else {
+//            String resultado = "";
+//         
+//
+//        }
     }
 }
